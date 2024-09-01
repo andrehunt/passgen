bc22d0a285b3d4e8fbf508a1ae05f91fc18ac21f
Andre <143305136+andrehunt@users.noreply.github.com>
2024-09-01 21:47:46 +1200

    Update script.js

--- script.js
+++ script.js
@@ -81,13 +81,7 @@ const strengthBar = document.getElementById('strengthBar');
 const themeToggleButton = document.getElementById('themeToggle');
 
 themeToggleButton.addEventListener('click', () => {
-    if (document.body.classList.contains('dark-mode')) {
-        document.body.classList.remove('dark-mode');
-        document.body.classList.add('light-mode');
-    } else {
-        document.body.classList.remove('light-mode');
-        document.body.classList.add('dark-mode');
-    }
+    document.body.classList.toggle('invert');
 });
 
 passwordType.addEventListener('change', function() {
